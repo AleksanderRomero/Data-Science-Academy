408ccf743f396b95eb648042f638038e7c64adbf
Aleksander <133550283+AleksanderRomero@users.noreply.github.com>
2024-06-21 11:33:53 -0300

    Update Studies

--- Fundamentos de Python para Data Science/Loops, Condicionais, Métodos e Funções/Lab2/calculadora_v1.py
+++ Fundamentos de Python para Data Science/Loops, Condicionais, Métodos e Funções/Lab2/calculadora_v1.py
@@ -1,8 +1,31 @@
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
-# A solução será apresentada no próximo capítulo!
-
 print("\n******************* Calculadora em Python *******************")
 
+print("\nSelecione o número da operação desejada: \n\n  1- Soma \n  2- Subtração \n  3- Multiplicação \n  4- Divisão")
+
+operacao = input("Digite sua opção (1/2/3/4):")
+
+numero1 = input("Digite o primeiro número:")
+
+numero2 = input("Digite o segundo número:")
+
+if operacao == "1":
+    resultado = int(numero1) + int(numero2)
+    print(resultado)
+
+elif operacao == "2":
+    resultado = int(numero1) - int(numero2)
+    print(resultado)
+
+elif operacao == "3":
+    resultado = int(numero1) * int(numero2)
+    print(resultado)
+
+elif operacao == "4":
+    resultado = int(numero1) / int(numero2)
+    print(resultado)
+    
+else:
+    print("\nOpção Inválida!")
 
